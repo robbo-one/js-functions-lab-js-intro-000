55d7e3b59119f46fa3b5f591f58f2d326e2c9d0c
R C <robsc801@gmail.com>
2020-11-02 02:10:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays(string){
   return "Happy holidays!";
 }
 function happyHolidaysTo(string) {
-  return `Happy holidays, ${name}!`;
+  return `"Happy holidays, ${name}!"`;
 }
 function happyCustomHolidayTo(string){
   return
